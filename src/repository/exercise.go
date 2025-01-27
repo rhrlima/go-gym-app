bdd532da5c7707ba3f281f1925421cafaf412615
Ricardo Lima <ricardo.hrlima@gmail.com>
2025-01-27 17:13:09 -0300

    refac: add missing file

--- src/repository/exercise.go
+++ src/repository/exercise.go
@@ -8,65 +8,27 @@ import (
 
 type ExerciseRepository struct {
 	connection *sql.DB
-	tagRepository TagRepository
-	exerciseTagRepository ExerciseTagRepository
 }
 
 func NewExerciseRepository(connection *sql.DB) ExerciseRepository {
 	return ExerciseRepository{
 		connection: connection,
-		tagRepository: NewTagRepository(connection),
-		exerciseTagRepository: NewExerciseTagRepository(connection),
 	}
 }
 
 func (er *ExerciseRepository) CreateExercise(exercise model.Exercise) (int, error) {
 
 	var id int
-	tx, err := er.connection.Begin()
+	query := "INSERT INTO exercises (name) VALUES ($1) RETURNING id"
+	stmt, err := er.connection.Prepare(query)
 	if err != nil {
 		return 0, err
 	}
+	defer stmt.Close()
 
-	// insert exercise
-	query1, err := er.connection.Prepare(
-		"INSERT INTO exercises (name) VALUES ($1) RETURNING id")
+	err = stmt.QueryRow(exercise.Name).Scan(&id)
 	if err != nil {
-		fmt.Println(err)
-		tx.Rollback()
 		return 0, err
-	}
-	defer query1.Close()
-
-	err = query1.QueryRow(exercise.Name).Scan(&id)
-	if err != nil {
-		fmt.Println(err)
-		tx.Rollback()
-		return 0, err
-	}
-
-	for _, tag := range exercise.Tags {
-		tag, err := er.tagRepository.GetTagByName(tag)
-		if err != nil {
-			tx.Rollback()
-			fmt.Println(err)
-			return 0, err
-		}
-
-		err = er.exerciseTagRepository.CreateExerciseTag(model.ExerciseTag{
-			ExerciseID: id,
-			TagID: tag.ID,
-		})
-		if err != nil {
-			tx.Rollback()
-			fmt.Println(err)
-			return 0, err
-		}
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return 0, nil
 	}
 
 	return id, nil
@@ -74,13 +36,14 @@ func (er *ExerciseRepository) CreateExercise(exercise model.Exercise) (int, erro
 
 func (er *ExerciseRepository) GetExercises() ([]model.Exercise, error) {
 
-	query := "SELECT id, name FROM exercises;"
+	query := "SELECT id, name FROM exercises"
 
 	rows, err := er.connection.Query(query)
 	if err != nil {
 		fmt.Println(err)
 		return []model.Exercise{}, err
 	}
+	defer rows.Close()
 
 	var exerciseList []model.Exercise
 	var exerciseObject model.Exercise
@@ -92,42 +55,27 @@ func (er *ExerciseRepository) GetExercises() ([]model.Exercise, error) {
 		)
 
 		if err != nil {
-			fmt.Println(err)
 			return []model.Exercise{}, err
 		}
 
 		exerciseList = append(exerciseList, exerciseObject)
-
-		exerciseTags, err := er.exerciseTagRepository.GetExerciseTagsByExerciseId(exerciseObject.ID)
-		if err != nil {
-			fmt.Println(err)
-			return []model.Exercise{}, err
-		}
-
-		tag_names := []string{}
-		for _, tag := range tags {
-			tag, err := er.tagRepository.GetTagByName(tag)
-			tag_names = append(tag_names, tag.Name)
-		}
-		exerciseObject.Tags = tags
 	}
-
-	rows.Close()
 
 	return exerciseList, nil
 }
 
-func (er *ExerciseRepository) GetExerciseById(id_exercise int) (*model.Exercise, error) {
+func (er *ExerciseRepository) GetExerciseById(exercise_id int) (*model.Exercise, error) {
 	query, err := er.connection.Prepare(
 		"SELECT * FROM exercises WHERE id = $1",
 	)
 	if err != nil {
 		return nil, err
 	}
+	defer query.Close()
 
 	var exercise model.Exercise
 
-	err = query.QueryRow(id_exercise).Scan(
+	err = query.QueryRow(exercise_id).Scan(
 		&exercise.ID,
 		&exercise.Name,
 	)
@@ -139,6 +87,5 @@ func (er *ExerciseRepository) GetExerciseById(id_exercise int) (*model.Exercise,
 		return nil, err
 	}
 
-	query.Close()
 	return &exercise, nil
 }
