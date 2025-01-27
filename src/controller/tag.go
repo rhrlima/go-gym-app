866415b922b1a7c3705587b6ba5ec5a7d456ec85
Ricardo Lima <ricardo.hrlima@gmail.com>
2025-01-27 17:12:31 -0300

    refac: tag endpoint

--- src/controller/tag.go
+++ src/controller/tag.go
@@ -9,17 +9,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type tagController struct {
+type TagController struct {
 	tagUsecase usecase.TagUsecase
 }
 
-func NewTagController(usecase usecase.TagUsecase) tagController {
-	return tagController{
+func NewTagController(usecase usecase.TagUsecase) TagController {
+	return TagController{
 		tagUsecase: usecase,
 	}
 }
 
-func (tc *tagController) CreateTag(ctx *gin.Context) {
+func (tc *TagController) CreateTag(ctx *gin.Context) {
 
 	var tag model.Tag
 	err := ctx.BindJSON(&tag)
@@ -37,7 +37,7 @@ func (tc *tagController) CreateTag(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, insertedTag)
 }
 
-func (tc *tagController) GetTags(ctx *gin.Context) {
+func (tc *TagController) GetTags(ctx *gin.Context) {
 
 	tags, err := tc.tagUsecase.GetTags()
 	if err != nil {
@@ -47,7 +47,7 @@ func (tc *tagController) GetTags(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, tags)
 }
 
-func (tc *tagController) GetTagByName(ctx *gin.Context) {
+func (tc *TagController) GetTagByName(ctx *gin.Context) {
 	tagName := ctx.Param("tagName")
 
 	if tagName == "" {
@@ -74,7 +74,7 @@ func (tc *tagController) GetTagByName(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, tag)
 }
 
-func (tc *tagController) GetTagsByExerciseId(ctx *gin.Context) {
+func (tc *TagController) GetTagsByExerciseId(ctx *gin.Context) {
 	id := ctx.Param("exerciseId")
 
 	if id == "" {
@@ -91,18 +91,12 @@ func (tc *tagController) GetTagsByExerciseId(ctx *gin.Context) {
 			Message: "Exercise Id must be an integer",
 		}
 		ctx.JSON(http.StatusBadRequest, response)
+		return
 	}
 	
 	tags, err := tc.tagUsecase.GetTagsByExerciseId(exerciseId)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, err)
-	}
-
-	if tags == nil {
-		response := model.Response{
-			Message: "Exercise ID not found",
-		}
-		ctx.JSON(http.StatusNotFound, response)
 		return
 	}
 
--- src/repository/tag.go
+++ src/repository/tag.go
@@ -19,9 +19,7 @@ func NewTagRepository(connection *sql.DB) TagRepository {
 func (tr *TagRepository) CreateTag(tag model.Tag) (int, error) {
 
 	var id int
-
 	query := "INSERT INTO tags (name) VALUES ($1) RETURNING id"
-
 	stmt, err := tr.connection.Prepare(query)
 	if err != nil {
 		return 0, err
@@ -94,11 +92,10 @@ func (tr *TagRepository) GetTagByName(name string) (*model.Tag, error) {
 }
 
 func (tr *TagRepository) GetTagsByExerciseId(exercise_id int) ([]model.Tag, error) {
-	query := "SELECT t.* FROM tags t JOIN exercisetags et ON t.id = et.tag_id WHERE et.exercise_id = $1;"
+	query := "SELECT t.* FROM tags t JOIN exercise_tags et ON t.id = et.tag_id WHERE et.exercise_id = $1;"
 
 	rows, err := tr.connection.Query(query, exercise_id)
 	if err != nil {
-		fmt.Println(err)
 		return []model.Tag{}, err
 	}
 	defer rows.Close()
@@ -113,19 +110,10 @@ func (tr *TagRepository) GetTagsByExerciseId(exercise_id int) ([]model.Tag, erro
 		)
 
 		if err != nil {
-			fmt.Println(err)
 			return []model.Tag{}, err
 		}
 
 		tagList = append(tagList, tag)
-	}
-
-	if err != nil {
-		fmt.Println(err)
-		if err == sql.ErrNoRows {
-			return nil, nil
-		}
-		return nil, err
 	}
 
 	return tagList, nil
